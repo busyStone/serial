1f042322e9ced949ac589363343bc1e5289f547a
busyStone <jlu.xzp@gmail.com>
2017-01-08 18:37:06 +0800

    errno == 0 should be no error

--- serial_linux.go
+++ serial_linux.go
@@ -143,12 +143,18 @@ func (p *Port) Write(b []byte) (n int, err error) {
 // or data received but not read
 func (p *Port) Flush() error {
 	const TCFLSH = 0x540B
-	_, _, err := syscall.Syscall(
+	_, _, errno := syscall.Syscall(
 		syscall.SYS_IOCTL,
 		uintptr(p.f.Fd()),
 		uintptr(TCFLSH),
 		uintptr(syscall.TCIOFLUSH),
 	)
+
+	err := nil
+	if errno != 0 {
+		err = errno
+	}
+
 	return err
 }
 
